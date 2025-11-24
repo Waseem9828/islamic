7e705fdb81f45380d960e165087d10ac1d64e4c4
Waseem Akram <khalilah64780@gmail.com>
2025-11-24 15:45:07 +0000

    Aapane Colors Mein changing Kar Di Hai Vahi purana color rakhiye jo abhi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,9 @@ export default function RootLayout({
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Amiri:wght@400;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.cdnfonts.com/css/jameel-noori-nastaleeq" rel="stylesheet" />
-
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
       </head>
-      <body className='font-roboto'>
+      <body>
         <Providers>
           <SidebarProvider>
             <ClientLayout>{children}</ClientLayout>
