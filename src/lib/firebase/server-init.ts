e149782dbcbd6a7ac9b99ef5942b061259f7f8e4
Waseem Akram <khalilah64780@gmail.com>
2025-11-26 01:27:09 +0000

    Try fixing this error: `Runtime Error: Error: The default Firebase app d

--- src/lib/firebase/server-init.ts
+++ src/lib/firebase/server-init.ts
@@ -1,17 +1,12 @@
+
 import * as admin from 'firebase-admin';
 
 // A function to safely initialize the Firebase Admin SDK and return the services.
 // This ensures that initializeApp() is called only once.
 function initializeServerApp() {
   if (!admin.apps.length) {
-    try {
-      // In a Google Cloud environment, the SDK can be initialized without credentials.
-      admin.initializeApp();
-    } catch (e) {
-      console.error('Firebase admin initialization error', e);
-      // If you were running locally and NOT in a Google Cloud environment,
-      // you would need to provide service account credentials here.
-    }
+    // In a Google Cloud environment, the SDK can be initialized without credentials.
+    admin.initializeApp();
   }
   return {
     auth: admin.auth(),
