f51d1038ed799f48a73ef698afe2624d1ab469b2
Waseem Akram <khalilah64780@gmail.com>
2025-11-29 07:00:02 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAHRqi6FiM0jjMIqX0j7Jwj91s0JLyAKak",
-  authDomain: "studio-4431476254-c1156.firebaseapp.com",
-  projectId: "studio-4431476254-c1156",
-  storageBucket: "studio-4431476254-c1156.firebasestorage.app",
-  messagingSenderId: "23513776021",
-  appId: "1:23513776021:web:3e5b6870112641c0fac09c",
+  "projectId": "studio-4431476254-c1156",
+  "appId": "1:23513776021:web:3e5b6870112641c0fac09c",
+  "apiKey": "AIzaSyAHRqi6FiM0jjMIqX0j7Jwj91s0JLyAKak",
+  "authDomain": "studio-4431476254-c1156.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "23513776021",
+  "storageBucket": "studio-4431476254-c1156.appspot.com"
 };
