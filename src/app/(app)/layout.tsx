d1d9ea440fa6289868c2cbc8f74be62adcd89856
Waseem Akram <khalilah64780@gmail.com>
2025-11-29 01:30:38 +0000

    ## Error Type Runtime Error
    ## Error Message
    useSidebar must be used wi

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,6 +2,7 @@
 
 import AppHeader from '@/components/layout/AppHeader';
 import Footer from '@/components/layout/footer';
+import { SidebarProvider } from '@/components/ui/sidebar';
 
 export default function AppLayout({
   children,
@@ -9,14 +10,16 @@ export default function AppLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <AppHeader />
-      <div className="flex flex-col sm:gap-4 sm:py-4">
-        <main className="flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-          {children}
-        </main>
+    <SidebarProvider>
+      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+        <AppHeader />
+        <div className="flex flex-col sm:gap-4 sm:py-4">
+          <main className="flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+            {children}
+          </main>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </SidebarProvider>
   );
 }
