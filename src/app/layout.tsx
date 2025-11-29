59afb1f462f2d1bb6c952c63878928e3d9ca7ff4
Waseem Akram <khalilah64780@gmail.com>
2025-11-29 01:48:44 +0000

    Hamara app abhi bhi bahut slow work kar raha hai

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import './globals.css';
 import { Providers } from './providers';
 import { Toaster } from 'sonner';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  variable: '--font-poppins',
+});
 
 export default function RootLayout({
   children,
@@ -8,16 +15,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.variable}>
       <head>
         <title>Islamic Random Selector</title>
         <meta
           name="description"
           content="A fair and random number selector with an Islamic theme."
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
       </head>
       <body>
         <Providers>
